773bfe9f9a11c9b6eb1f7df93a083f12443f1679
Mosawer Mahboob <masomahboob@proton.me>
2023-10-04 01:38:30 +0430

    Configure MEDIA URL/ROOT for the project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = None
+    id_user = None
+    bio = None
+    profile_image = models.ImageField()
+    location = None
